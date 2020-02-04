adb0c868109a1494b819bd429781c60877673079
Fabio Oquendo <fabio.oquendo@xpertgroup.co>
2020-02-04 11:39:16 -0500

    crear entidades en dominio

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -34,9 +34,9 @@ namespace CoreEscuela.Entidades {
         }
         private List<Curso> CargarCursos () {
             Escuela.Cursos = new List<Curso> () {
-                new Curso () { Nombre = "101", Jornada = TiposJornada.Mañana },
-                new Curso () { Nombre = "201", Jornada = TiposJornada.Mañana },
-                new Curso () { Nombre = "301", Jornada = TiposJornada.Mañana },
+                new Curso () { Nombre = "101", Jornada = TiposJornada.Manana },
+                new Curso () { Nombre = "201", Jornada = TiposJornada.Manana },
+                new Curso () { Nombre = "301", Jornada = TiposJornada.Manana },
                 new Curso () { Nombre = "401", Jornada = TiposJornada.Tarde },
                 new Curso () { Nombre = "501", Jornada = TiposJornada.Tarde },
             };
