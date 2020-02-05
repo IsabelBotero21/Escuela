a2aaa45d24ee43d18038379fa4fec84031859737
Fabio Oquendo <fabio.oquendo@xpertgroup.co>
2020-02-05 13:46:33 -0500

    Repositorios en AccesoDatos

--- AccesoDatos/EscuelaRepositorio.cs
+++ AccesoDatos/EscuelaRepositorio.cs
@@ -1,13 +1,10 @@
 using CoreEscuela.Entidades;
 
-namespace Etapa1.AccesoDatos
-{
-    public class EscuelaRepositorio
-    {
-            public Escuela Escuela { get; set; }
-        public void Inicializar()
-        {
-            Escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, Pais: "Colombia", Ciudad: "Abejorral");
+namespace Etapa1.AccesoDatos {
+    public class EscuelaRepositorio {
+        public Escuela Escuela { get; set; }
+        public void Inicializar () {
+            Escuela = new Escuela ("Platzi Academy", 2012, TiposEscuela.Primaria, Pais: "Colombia", Ciudad: "Abejorral");
 
         }
     }
