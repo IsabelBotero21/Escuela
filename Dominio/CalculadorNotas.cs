c0caffb3c060267ca4b5375d536dc374ecb7208a
Fabio Oquendo <fabio.oquendo@xpertgroup.co>
2020-02-06 09:50:31 -0500

    notas

--- Dominio/CalculadorNotas.cs
+++ Dominio/CalculadorNotas.cs
@@ -1,11 +1,18 @@
 using CoreEscuela.Entidades;
+using Etapa1.AccesoDatos;
 
 namespace Etapa1.Dominio {
     public class CalculadorNotas {
+        public Escuela Escuela { get; set; }
         private void CalcularNotaMasAlta (Curso curso) {
+        var cursos = new CursoRepositorio().ObtenerCursos(); 
+        for (int i = 0; i < 5; i++)
+        {
+           
 
         }
 
+        }
         private void CalcularPromedioNotas (Curso curso) { }
 
     }
