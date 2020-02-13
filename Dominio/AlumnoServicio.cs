5ee09cc19d41b43c579086774039dbdb43c98985
Fabio Oquendo <fabio.oquendo@xpertgroup.co>
2020-02-13 08:24:38 -0500

    AlumnoNotaMasAlta

--- Dominio/AlumnoServicio.cs
+++ Dominio/AlumnoServicio.cs
@@ -1,11 +1,26 @@
-
 using System.Collections.Generic;
 using CoreEscuela.Entidades;
 
-namespace Etapa1.Dominio
-{
-    public class AlumnoServicio
-    {
+namespace Etapa1.Dominio {
+    public class AlumnoServicio {
+        //Traer alumnos
+        public float alumnoMayorNota (Escuela escuela) {
+            string alumnoConMayorNota = null;
+            float notaMasAlta = 0;
+            var array = new List<Alumno> ();
+            escuela.Cursos.ForEach (curso => {
+                curso.Alumno.ForEach (alumno => {
+                    alumno.Evaluacion.ForEach (evaluacion => {
+                        array.Add (alumno);
+                        if (evaluacion.Nota > notaMasAlta)
+                        {
+                            notaMasAlta = evaluacion.Nota;
+                            alumnoConMayorNota = alumno;
+                        }
+                    });
+                });
+            });
+            return notaMasAlta;
+        }
     }
 }
-    
